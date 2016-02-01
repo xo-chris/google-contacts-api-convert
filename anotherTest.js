6759a2375847b55585ca0f3df4ee4942cfdc2aa0
kiwichris1 <chris@exosphereapps.com>
2016-02-01 18:32:48 +1300

    Update anotherTest.js

--- anotherTest.js
+++ anotherTest.js
@@ -1,28 +1,28 @@
-//download a single contact, convert the XML and submit to the google api
 
+//modules required
 var request = require("request");
 var xml2js = require('xml2js');
 var async = require("async");
 
 //global variables
-var maxContacts = 10;
-var googleUser = "chris@exosphereapps.com";
+var maxContacts = 10; //arbitrary number, I am just testing with two contacts
+var googleUser = "chris@exosphereapps.com"; //copy contacts from this user
 var token = "ya29.ewL9-6kO10a9xAstgS1ntTVkp3qDiPKQeSAsqBumjIRA3rwhu_g2O8s7yoJNdCu35vi_";
 
-var targetGoogleUser = "fred@exosphereapps.com";
+var targetGoogleUser = "fred@exosphereapps.com"; //copy contacts to this user
 var targetToken = 'ya29.ewL_KFwrhL1ujAXAE26Ow5NXb0pAeiFTT9u6I3Ml7pAmAm62e76S4T6ERYV7T3jvHQZD3xGMu0n6NslsS8BfXhAYS-AivuPeQ1P8885UdYUK43PK1g';
 
 
 /*
-add all contacts with 'xo_sync' present in a field e.g. the contact notes or a custom field
+add all contacts with 'xo_sync' present in a field e.g. in a custom field
 
 expected result:  I have two contacts with 'xo_sync' present.  I would expect both contacts to be added to the target google user's contacts
 
-what actually happens (for me): the parsed XML contains three <entry> nodes (I would expect two) and a single, blank contact is added
+what actually happens (for me): the parsed XML contains three <entry> nodes (I would expect two) and a single, blank contact is added to the target user's contacts
 
 
 TODO:  write an 'update contacts' function that would get all contacts with 'xo_sync' present, modify them e.g. change the name and then submit
-the update back to the api
+the update back to the api - this will test that the update syntax returned is correct
 */
 addContacts("xo_sync");
 
@@ -75,7 +75,7 @@ function convertContact(XML,callback){
   var convert = require('./converter.js');
 
   var aXML = [];
-  aXML.push(XML)
+  aXML.push(XML) //so we can pass in an array.  Not sure if I am using the convert function correctly here??
   convert(aXML, function(err,response) {
 
     if (err) return error(err);
