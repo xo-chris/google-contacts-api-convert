7765afea7b0b54091c32ab3e6ee8e01087dea772
kiwichris1 <chris@exosphereapps.com>
2016-02-01 18:26:12 +1300

    Update anotherTest.js

--- anotherTest.js
+++ anotherTest.js
@@ -5,29 +5,44 @@ var xml2js = require('xml2js');
 var async = require("async");
 
 //global variables
-var maxContactsPerRequest = 1;
-var q = "chris@exosphereapps.com";
+var maxContacts = 10;
 var googleUser = "chris@exosphereapps.com";
+var token = "ya29.ewL9-6kO10a9xAstgS1ntTVkp3qDiPKQeSAsqBumjIRA3rwhu_g2O8s7yoJNdCu35vi_";
 
-var token = "ya29.egKoXccp5pKj1F7WflGgWExpHYuTMJkiWDuXIimtWkJ5YLGt1z6059I51EH4JDeqRmd14XIRAh4JsF-MVYeV7b5KAlxKt0LmTUkM-SRRSHSADXvfAP0";
-
-
-
-async.waterfall([
-  getContact,
-  convertContact,
-  submitContact
-],
-// this gets called at the end
-function( err){
-  if (err) throw err;
-});
+var targetGoogleUser = "fred@exosphereapps.com";
+var targetToken = 'ya29.ewL_KFwrhL1ujAXAE26Ow5NXb0pAeiFTT9u6I3Ml7pAmAm62e76S4T6ERYV7T3jvHQZD3xGMu0n6NslsS8BfXhAYS-AivuPeQ1P8885UdYUK43PK1g';
 
 
-function getContact(callback){
+/*
+add all contacts with 'xo_sync' present in a field e.g. the contact notes or a custom field
+
+expected result:  I have two contacts with 'xo_sync' present.  I would expect both contacts to be added to the target google user's contacts
+
+what actually happens (for me): the parsed XML contains three <entry> nodes (I would expect two) and a single, blank contact is added
+
+
+TODO:  write an 'update contacts' function that would get all contacts with 'xo_sync' present, modify them e.g. change the name and then submit
+the update back to the api
+*/
+addContacts("xo_sync");
+
+
+function addContacts(query){
+  async.waterfall([
+    getContact.bind(null,query),
+    convertContact,
+    submitContact
+  ],
+  // this gets called at the end
+  function( err){
+    if (err) throw err;
+  });
+}
+
+function getContact(query,callback){
 
   console.log('getting contact');
-  var url =  'https://www.google.com/m8/feeds/contacts/' + googleUser + '/full/?alt=atom&max-results='+maxContactsPerRequest+'&q=' + q;
+  var url =  'https://www.google.com/m8/feeds/contacts/' + googleUser + '/full/?alt=atom&max-results='+maxContacts + '&q=' + query;
 
   var options = {
     url: url,
@@ -44,6 +59,8 @@ function getContact(callback){
 
     if (response.statusCode != "200")
     throw ("response code from " + url + " was not 200.  Received " + res.statusCode + " " + res.statusMessage);
+
+    console.log("GOT CONTACTS OK " + body);
 
     callback(null,body);
 
@@ -72,13 +89,13 @@ function submitContact(parsedXML,callback){
 
   console.log("submitting " + parsedXML);
 
-  var url =  'https://www.google.com/m8/feeds/contacts/' + googleUser + '/full/';
+  var url =  'https://www.google.com/m8/feeds/contacts/' + targetGoogleUser + '/full/';
 
   var options =
   {
     method: "POST",
     gzip: true,
-    headers:  {'Authorization': "Bearer " + token,
+    headers:  {'Authorization': "Bearer " + targetToken,
               'Content-Type': 'application/atom+xml',
               'GData-Version': '3.0'},
     body:parsedXML,
@@ -101,4 +118,3 @@ function submitContact(parsedXML,callback){
   });
 
 }
-
